a36fe1797069e853b8364b01639c9b6639e3f576
Ahmet Sina Ustem <asend.ustem@gmail.com>
2018-06-30 23:42:10 +0300

    Initial Commit

---
+++ MaoTrailer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MaoTrailer
+//
+//  Created by Ahmet Sina on 30.06.2018.
+//  Copyright © 2018 Ahmet Sina Ustem. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
